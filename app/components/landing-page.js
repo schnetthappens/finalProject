45716c35163393c883d387fc1e32462310779c6b
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-07 15:30:12 -0400

    Console.log tryDemo action on button

--- app/components/landing-page.js
+++ app/components/landing-page.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['landing-page-container']
+  classNames: ['landing-page-container'],
+
+  actions: {
+    tryDemo: function(){
+      console.log('demo');
+    }
+  }
 });
