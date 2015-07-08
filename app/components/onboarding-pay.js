ac5d535181dcc189dbafd55cbbc17c36a9eda4f3
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-08 16:17:19 -0400

    Store Stripe token on submit

--- app/components/onboarding-pay.js
+++ app/components/onboarding-pay.js
@@ -2,5 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['connect-payment-container']
+  classNames: ['connect-payment-container'],
+
+  didInsertElement: function(){
+    this.handler = StripeCheckout.configure({
+      key: "pk_test_qtYTRfTiY5i8Ja2JapoUGGlN",
+      image: "https://stripe.com/img/documentation/checkout/marketplace.png",
+      token: this.handleToken
+    });
+  },
+
+  handleToken: function(token) {
+    console.log('handleToken', token);
+  },
+
+  actions: {
+    checkout: function() {
+      this.handler.open({
+        name: "Cool Company",
+        description: "Forgetting to enter time",
+        amount: "100"
+      });
+    }
+  }
 });
