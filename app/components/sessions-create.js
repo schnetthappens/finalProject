83b4094938c0ec4c0737a9a077f741d534b30550
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-09 14:31:50 -0400

    Implement UnauthenticatedRouteMixin when authenticated

--- app/components/sessions-create.js
+++ app/components/sessions-create.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
-import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Component.extend(LoginControllerMixin, UnauthenticatedRouteMixin, {
+
+export default Ember.Component.extend(LoginControllerMixin, {
   tagName: 'div',
   classNames: ['login-container'],
   authenticator: 'authenticator:parse-username'
