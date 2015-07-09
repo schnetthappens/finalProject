495d896d408d50db097fed687d1189f5169b0670
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-09 16:50:17 -0400

    Implement authentication sign up

--- app/components/users-create.js
+++ app/components/users-create.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['create-user-container']
+  classNames: ['create-user-container'],
+
+  actions: {
+    createUser: function() {
+      this.sendAction('action', this.get('model'));
+    }
+  }
 });
--- app/routes/users/create.js
+++ app/routes/users/create.js
@@ -2,4 +2,20 @@ import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+
+  model: function(){
+    return this.store.createRecord('parseUser');
+  },
+
+  actions: {
+    createUser: function(user){
+        user.set('email', user.get('username'));
+
+        user.save().then(function(){
+          this.get('session').authenticate('authenticator:parse-token', {
+            sessionToken: user.get('sessionToken')
+          });
+        }.bind(this));
+      }
+    }
 });
