ff819b9154e55ab62a17321703d066053a81f1fb
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-11 11:51:38 -0400

    Implement current user styles

--- app/components/users-current.js
+++ app/components/users-current.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['current-user-container'],
+
   actions: {
     invalidateSession: function(){
       this.sendAction('action', this.get('model'));
