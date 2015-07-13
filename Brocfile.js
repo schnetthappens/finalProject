fff682b22c97f87321667637a0d4bd5442bf43fb
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-13 09:28:43 -0400

    Import underscore.js in Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -2,10 +2,7 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp({
-  sourcemaps: {enabled: false}
-});
-
+var app = new EmberApp();
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -19,5 +16,7 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
+app.import('bower_components/underscore/underscore-mins.js');
 
 module.exports = app.toTree();
