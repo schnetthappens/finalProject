4fa50d99c7edcbefd5019fc37fb89b6f305ac874
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-13 21:52:30 -0400

    Returning multiple ajax calls on model

--- app/routes/users/show.js
+++ app/routes/users/show.js
@@ -4,16 +4,18 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(){
     var token = window.location.hash.match(/access_token=([^&]+)/)[1];
-      return Ember.$.ajax({
-        url: 'https://jsonp.afeld.me/?callback=?&url=https://api.harvestapp.com/account/who_am_i?access_token='+token,
-        dataType: 'jsonp'
-      }).then(function(response) {
-        console.log(response);
-        return response;
-      }, function(xhr, status, error) {
-        console.log(error);
+      return Ember.RSVP.hash({
+        user: Ember.$.ajax({
+          url: 'https://jsonp.afeld.me/?callback=?&url=https://api.harvestapp.com/account/who_am_i?access_token='+token,
+          dataType: 'jsonp'
+        }),
+
+        entries: Ember.$.ajax({
+          url: 'https://jsonp.afeld.me/?callback=?&url=https://api.harvestapp.com/account/who_am_i?access_token='+token,
+          dataType: 'jsonp'
+        })
       });
-    },
+  },
 
   actions: {
     invalidateSession: function() {
