ce3052e956e9d498b96a37df44dc903cd328d2fc
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-16 13:50:43 -0400

    Saving org to Parse user from console

--- app/routes/users/current.js
+++ app/routes/users/current.js
@@ -8,16 +8,22 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   },
 
   actions: {
-    saveOrg: function(){
+    saveOrg: function(data){
       var o = $(".select-organization-list option:selected").val();
-      console.log(o);
+      var adapter = this.store.adapterFor('application');
+      adapter.ajax("https://api.parse.com/1/functions/setUserData", "POST", {
+        data: data
+      }).then(function(response) {
+        //do something once it's resolved
+        console.log(response);
+      });
     }
   }
 });
 
 
 /*
-// this.send('setUserData', {harvestToken: 'actualtoken'});
+
 var adapter = this.store.adapterFor('application');
 adapter.ajax("https://api.parse.com/1/functions/setUserData", "POST", {
   data: data
