6d3578c1e89ba2029966c5be3dbd791ca9f037fd
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-18 16:29:29 -0400

    Update Stripe form with logo, include comments

--- app/components/users-show-pay.js
+++ app/components/users-show-pay.js
@@ -4,25 +4,29 @@ export default Ember.Component.extend({
   tagName: 'div',
   classNames: ['user-show-pay-container'],
 
+  //Creates the Checkout form when the element is inserted into the DOM
   didInsertElement: function(){
     this.handler = StripeCheckout.configure({
       key: "pk_test_qtYTRfTiY5i8Ja2JapoUGGlN",
-      image: "https://stripe.com/img/documentation/checkout/marketplace.png",
+      image: "assets/images/timesweep_logo.png",
       token: this.handleToken
     });
   },
 
+  //Handles the token to create the charge
   handleToken: function(token) {
     console.log('handleToken', token);
   },
 
   actions: {
+    
+    //Opens the Checkout form when button is clicked
     checkout: function() {
       this.handler.open({
         name: "Timesweep.io",
         description: "Just keeping you honest!",
         amount: "100",
-        panelLabel: 'Save Card'
+        panelLabel: 'Pay'
       });
     }
   }
