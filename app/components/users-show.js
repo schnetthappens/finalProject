29995a25a8b349b0d81a08dc8497e5f4acff263f
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-18 16:11:43 -0400

    Add class names to components

--- app/components/users-show.js
+++ app/components/users-show.js
@@ -2,31 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['users-show-container'],
-
-  didInsertElement: function(){
-    this.handler = StripeCheckout.configure({
-      key: "pk_test_qtYTRfTiY5i8Ja2JapoUGGlN",
-      image: "https://stripe.com/img/documentation/checkout/marketplace.png",
-      token: this.handleToken
-    });
-  },
-
-  handleToken: function(token) {
-    console.log('handleToken', token);
-  },
-
-  actions: {
-  
-    checkout: function() {
-      this.handler.open({
-        name: "Timesweep.io",
-        description: "Just keeping you honest!",
-        amount: "100",
-        panelLabel: 'Save Card'
-      });
-    }
-  }
+  classNames: ['users-show-container']
 });
 
 /*
