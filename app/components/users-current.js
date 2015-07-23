e79b865d9563a728a64c43f7d0554b39a9e1c707
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-22 20:12:45 -0400

    Fix styles on ddslick plugin

--- app/components/users-current.js
+++ app/components/users-current.js
@@ -11,28 +11,24 @@ export default Ember.Component.extend({
             text: "Girl Develop It",
             value: 0,
             selected: false,
-            description: "Girl Develop It is a nonprofit organization that exists to provide affordable and judgment-free opportunities for women interested in learning web and software development.",
             imageSrc: "../assets/images/gdi_logo.png"
         },
         {
             text: "100 Girls of Code",
             value: 1,
             selected: false,
-            description: "The mission of 100 Girls of Code is to achieve gender parity in STEM fields by introducing more young women to code and computer engineering at a young age.",
             imageSrc: "../assets/images/girlsofcode.png"
         },
         {
             text: "Girls Who Code",
             value: 2,
             selected: false,
-            description: "Girls Who Code programs work to inspire, educate, and equip girls with the computing skills to pursue 21st century opportunities.",
             imageSrc: "../assets/images/girlswhocode.png"
         },
         {
             text: "Code.org",
             value: 3,
             selected: false,
-            description: "Code.org® is a non-profit dedicated to expanding participation in computer science by making it available in more schools, and increasing participation by women and underrepresented students of color.",
             imageSrc: "../assets/images/codeorg.png"
         }
     ];
