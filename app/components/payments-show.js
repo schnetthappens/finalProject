bd485a04e266f65cfefc1888e2231e74bf35d2f1
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-07-23 16:41:30 -0400

    Implement modal on payments/show route

--- app/components/payments-show.js
+++ app/components/payments-show.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
-  classNames: ['payment-show-container']
+  classNames: ['payment-show-container'],
+
+  didInsertElement: function(){
+    $(document).ready(function(){
+      $('.modal-trigger').leanModal();
+    });
+  }
 });
